7880db3567c4bc4368e783f34dff067e957bdf03
Dmitri <dmitri.antipov1@gmail.com>
2022-09-14 18:38:43 +0300

    make responsive

--- src/app/components/icons/triangle/triangle.component.ts
+++ src/app/components/icons/triangle/triangle.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-triangle',
   templateUrl: './triangle.component.html',
   styleUrls: ['./triangle.component.css']
 })
-export class TriangleComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class TriangleComponent {
 
 }
